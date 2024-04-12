1a3b2c9af88f979b249b7a81f0770f7cdf6083b3
davydjulian <davidjulian@outlook.es>
2024-04-11 19:22:26 -0500

    ajuste variables

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQytcPetrvKp8iw7wFFO3Mb96ZbNL1Tco",
-  authDomain: "backendloots.firebaseapp.com",
-  projectId: "backendloots",
-  storageBucket: "backendloots.appspot.com",
-  messagingSenderId: "174369843320",
-  appId: "1:174369843320:web:056cbb2383ccf29e7b658d"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
